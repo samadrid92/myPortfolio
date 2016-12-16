275541898ec3f9492a87e82137a4e21c785b917e
Scott A Madrid <samadrid@gmail.com>
2016-12-16 13:26:37 -0800

    got index working the way intended everything working great

--- scripts/app.js
+++ scripts/app.js
@@ -1,59 +1,35 @@
-console.log("sanity check");
+console.log("App.js Working");
+var windowHref;
+var currentArray;
+var currentHref;
+//gets current href of user
+windowHref = window.location.href;
+currentArray = windowHref.split("/")
+currentHref = currentArray[currentArray.length-1];
 
-var i;
-var $spot = $("#me-section");
-var counter = 0;
-var nums = ["one","two","three","four","five","six"];
+//statement that will run function only on index.html
+if ($('a.my-logo').attr('href') === currentHref){
+  // playDescriptions();
+}
 
-
-// for (i=0; i < nums.length; i++){
-//   var oneItem = nums[i];
-//   $spot.append(oneItem);
-//
-//   // console.log();
-// }
-
-// $.each(nums, function (i, v) {
-//   var array = v;
-//   console.log(array);
-// })
-
-
-
-// function boo(array){
-//   for (i=0; i < nums.length; i++){
-//       $spot.replaceWith(nums[i] + "<br>");
-//   }
-//
-// }
-
-var i = 0;
-var $spot = $("#me-section");
-var nums = ["Web Developer","Web Designer","L.A. Rams Fan","L.A. Dodgers Fan","L.A. Kings Fan",
-            "L.A. Lakers Fan", "1st Gen American", "Foodie", "Dog Lover", "Wanderlust"];
-
-// setInterval(function(){
-//   $spot.empty();
-//   for (i;;){
-//     var count = i;
-//     console.log("count:" + count);
-//     $spot.append("<h1>" + nums[count] + "</h1>");
-//     break;
-//   }
-//   i++;
-// }, 1000);
-
-
-
-// function boom(){
-//   alert("boom!");
-// }
-//
-// setTimeout(boom, 2000);
-
-
-
-
+function playDescriptions(){
+  var i = 0;
+  var $homeDiv = $("#me-div");
+  var descriptions = ["Web Developer","Web Designer","L.A. Rams Fan","L.A. Dodgers Fan","L.A. Kings Fan",
+                      "L.A. Lakers Fan", "1st Gen American", "Foodie", "Dog Lover", "Wanderlust"];
+  setInterval(function(){
+    $homeDiv.empty();
+    if (i >= descriptions.length ){
+      i = 0;
+    }
+    for (i;;){
+      var count = i;
+      $homeDiv.append("<h1>" + descriptions[count] + "</h1>");
+      break;
+    }
+    i++;
+  }, 700);
+}
 
 
 
@@ -71,12 +47,3 @@ function playQualities() {
     alert("It's running on index.html!");
   }
 }
-
-// function getRandom(arr){
-//   var rand = Math.random();
-//   return arr[Math.floor(rand *arr.length)];
-// }
-
-
-// var words = "developer"
-// $("#me-section").html(words);
